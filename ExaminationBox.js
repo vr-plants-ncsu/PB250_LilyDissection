99716320d1cd740d3b34f0a9dd4f008764720c07
Glitch (hello-webpage) <none>
2021-03-08 19:31:10 +0000

    🏃🍡 Checkpoint ./ExaminationBox.js:28363934/534 ./index.html:28363934/115

--- ExaminationBox.js
+++ ExaminationBox.js
@@ -1,9 +1,12 @@
 var castPoint;
 var trackedList;
 var storedObject;
+var canAssociate = true;
 
-AFRAME.registerComponent('examBox',{
-  schema: {},
+AFRAME.registerComponent('exambox',{
+  schema: {
+    snapedRotation: {type: 'vec3'}
+  },
   init: function (){
     //find the point we're going to cast from
     castPoint = document.querySelector('#castPoint');
@@ -12,10 +15,16 @@ AFRAME.registerComponent('examBox',{
       console.log("No cast point found! Add one to the entity that has the exam box");
     }
     //check for tagged entries and add them to our check list
-    trackedList = document.querySelectorAll('.examinable');
+    trackedList = document.querySelectorAll('.collidable');
   },
   tick: function(){
   //get the location of all the entities in our check list
+    if(!canAssociate){
+      if(storedObject.object3D.position.distanceTo(castPoint.object3D.position) > 0.5){
+        this.disassociate();
+      }
+      return;
+    }
     trackedList.forEach(element =>{
       if(element.object3D.position.distanceTo(castPoint.object3D.position) < 0.5){
         this.associate(element);
@@ -27,8 +36,14 @@ AFRAME.registerComponent('examBox',{
 },
   associate: function(entity){
     //move the element to the point
+    entity.object3D.position = castPoint.object3D.position;
     //rotate to our ideal rotation
     //set to ideal scale
     //make it impossible to associate other objects
+    canAssociate = false;
+  },
+  disassociate: function(){
+    storedObject = null;
+    canAssociate = true;
   }
 });
