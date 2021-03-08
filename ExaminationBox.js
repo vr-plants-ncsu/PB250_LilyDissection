517741abd57a3c7d838be1fab7b035abdb0ce8c8
Glitch (hello-webpage) <none>
2021-03-08 16:56:23 +0000

    💄🔖 Checkpoint ./ExaminationBox.js:28363934/597

---
+++ ExaminationBox.js
@@ -0,0 +1,14 @@
+AFRAME.registerComponent('examBox',{
+  schema: {},
+  init: function (){
+    //find the point we're going to cast from
+    //if we don't have it log it
+    //check for tagged entries and add them to our check list
+  },
+  tick: function(){
+  //get the location of all the entities in our check list
+  //if any of them are close enough to our cast point and the box is empty
+    //make that object our stored object and require it be removed before 
+    //adding any other new objects
+}
+});
