9250f76e0dd35821d1829ae630794ad3fda63dc8
Glitch (hello-webpage) <none>
2021-03-23 15:39:52 +0000

    ⛄️🎪 Checkpoint ./index.html:28363934/59 ./Examinable.js:28363934/27

--- Examinable.js
+++ Examinable.js
@@ -4,9 +4,10 @@ AFRAME.registerComponent('examinable',{
     snapedScale: {type: 'vec3'}
   },
  init: function(){
+   var entity = this.el;
    this.el.addEventListener('click', function(evt){
      //find our examination box
-     var examBoxComp = document.querySelector('ExamBox').components.exambox;
+     var examBoxComp = document.querySelector('[ExamBox]').components.exambox;
      //associate it
      examBoxComp.associate(this.el);
    })
