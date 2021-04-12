88b9e4cc68ad89d5c0e7b14439640a70cbffdc65
Glitch (pb250-lily-feature) <none>
2021-04-12 22:38:37 +0000

    🍈📇 Checkpoint ./ExamineCallout.js:28363934/146

--- ExamineCallout.js
+++ ExamineCallout.js
@@ -7,12 +7,12 @@ AFRAME.registerComponent('examinecallout',{
     defContent: {type:'string', default:"Click on a part of the plant for more information\n test test"}
   },
   init: function(){
-    header = document.querySelector('[examinetextheader]');
-    context = document.querySelector('[examinetextbody]');
+    header = document.querySelector('[examinetextheader]').components.text;
+    context = document.querySelector('[examinetextbody]').components.text;
     
     let exambox = document.querySelector('[ExamBox]');
-    exambox.addEventListener('associated', this.whenAssociated);
-   exambox.addEventListener('disassociated', this.whenDisassociated);
+    exambox.addEventListener('associated', function(){console.log("Yo");});
+    exambox.addEventListener('disassociated', this.whenDisassociated);
   },
   whenAssociated: function(event){
     console.log("Yo");
@@ -20,6 +20,7 @@ AFRAME.registerComponent('examinecallout',{
     context.value = event.detail.associatedEntity.components.examinable.data.contentText;
   },
   whenDisassociated: function(event){
+    console.log("Yo2");
     header.value = this.data.defHeader;
     content.value = this.data.defContent;
   }
