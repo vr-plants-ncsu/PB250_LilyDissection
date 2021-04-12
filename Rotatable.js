99ff68934bf7be88420e4c0b1b85d43cfd246090
Glitch (pb250-lily-feature) <none>
2021-04-12 19:47:52 +0000

    🍉🐄 Checkpoint ./Rotatable.js:28363934/378

--- Rotatable.js
+++ Rotatable.js
@@ -5,11 +5,16 @@ AFRAME.registerComponent('rotatable',{
   },
   init: function(){
     this.el.addEventListener('click', this.startRotation);
+    document.addEventListener('mousedown', this.onMouseDown)
     document.addEventListener('mouseup', this.onMouseUp);
     document.addEventListener('mousemove', this.onMouseMove);
   },
   startRotation: function(){
     isRotating = true;
+  },
+  onMouseDown: function (evt) {
+    this.oldClientX = evt.clientX;
+    this.oldClientY = evt.clientY;
   },
   onMouseUp: function(){
     isRotating = false;
@@ -24,9 +29,11 @@ AFRAME.registerComponent('rotatable',{
     var dY;
     //TODO find our pivot
     var modelPivotEl = this.modelPivotEl;
+    //todo rectify
     if (!this.oldClientX) { return; }
     dX = this.oldClientX - evt.clientX;
     dY = this.oldClientY - evt.clientY;
+    //we can tweak these as speeds
     modelPivotEl.object3D.rotation.y -= dX / 100;
     modelPivotEl.object3D.rotation.x -= dY / 200;
 
