77e1e363f3fb7ad7c84f51861bf98c26604ed626
Glitch (pb250-lily-feature) <none>
2021-04-12 22:49:43 +0000

    ❣️🌪 Checkpoint ./ExamineCallout.js:28363934/679 ./index.html:28363934/4

--- ExamineCallout.js
+++ ExamineCallout.js
@@ -1,5 +1,5 @@
-var header;
-var content;
+var header = null;
+var content = null;
 
 AFRAME.registerComponent('examinecallout',{
   schema: {
@@ -7,21 +7,25 @@ AFRAME.registerComponent('examinecallout',{
     defContent: {type:'string', default:"Click on a part of the plant for more information\n test test"}
   },
   init: function(){
-    header = document.querySelector('[examinetextheader]').components.text;
-    context = document.querySelector('[examinetextbody]').components.text;
+    header = document.querySelector('#examinetextheader').components.text;
+    context = document.querySelector('#examinetextbody').components.text;
     
     let exambox = document.querySelector('[ExamBox]');
-    exambox.addEventListener('associated', function(){console.log("Yo");});
-    exambox.addEventListener('disassociated', this.whenDisassociated);
+    exambox.addEventListener('associated', function(event){
+      header.value = event.detail.associatedEntity.components.examinable.data.headerText;
+      context.value = event.detail.associatedEntity.components.examinable.data.contentText;
+    });
+    exambox.addEventListener('disassociated', function(){
+      header.value = this.data.defHeader;
+      content.value = this.data.defContent;
+    });
   },
   whenAssociated: function(event){
     console.log("Yo");
-    header.value = event.detail.associatedEntity.components.examinable.data.headerText;
-    context.value = event.detail.associatedEntity.components.examinable.data.contentText;
+    
   },
   whenDisassociated: function(event){
     console.log("Yo2");
-    header.value = this.data.defHeader;
-    content.value = this.data.defContent;
+    
   }
 });
