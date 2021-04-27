9175602926516b37d976fe80b236f19a8d1f59cd
Glitch (pb250-lily-feature) <none>
2021-04-27 19:34:40 +0000

    👾⭐️ Checkpoint ./ExaminationBox.js:28363934/6

--- ExaminationBox.js
+++ ExaminationBox.js
@@ -50,7 +50,7 @@ AFRAME.registerComponent('exambox',{
 
     //TweenMax.to(newEntity.object3D, 0.3, {three:{rotationX:this.data.snapedRotation.x, rotationY:this.data.snapedRotation.y, rotationZ:this.data.snapedRotation.z}, ease:Sine.easeIn});
     let offsetLocation = new THREE.Vector3(0,0,0);
-    offsetLocation.addVectors(this.el.object3D.position, this.data.snapedOffset);
+    //offsetLocation.addVectors(this.el.object3D.position, this.data.snapedOffset);
     TweenMax.to(newEntity.object3D, 0.3, {three:{positionX: offsetLocation.x, positionY: offsetLocation.y,positionZ: offsetLocation.z}, ease:Sine.easeIn});
     //TODO have a look here for positioning
     //childEn.setAttribute('position',{x: 1, y: 0, z:0});
