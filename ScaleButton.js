a200970b36642c69096e0ead821606bf717bb147
Glitch (pb250-lily-feature) <none>
2021-05-06 18:20:51 +0000

    ⛹🍈 Checkpoint ./index.html:28363934/51 ./ScaleButton.js:28363934/514

--- ScaleButton.js
+++ ScaleButton.js
@@ -1,19 +1,21 @@
 var target = null;
 var clickCooldownCounter = 0;
-var currentScale = 0.0;
+var entity = null;
 
 AFRAME.registerComponent('scalebutton',{
   schema: {
     clickCooldown:{type: 'float', default: 0.2},
     scaleDelta: {type: 'float', default: 0},
     scaleMin: {type: 'float', default: -1000},
-    scaleMax: {type: 'float', default: 1000}
+    scaleMax: {type: 'float', default: 1000},
+    currentScale: {type: 'float', default: 0}
   },
  init: function(){
    let examBoxComp = document.querySelector('[ExamBox]');
+   entity = this.el;
    examBoxComp.addEventListener('associated', this.whenAssociated);
    examBoxComp.addEventListener('disassociated', this.whenDisassociated);
-   let entity = this.el;
+   
    this.el.addEventListener('mousedown', function(evt){
      if(clickCooldownCounter > 0){
        return;
@@ -21,8 +23,8 @@ AFRAME.registerComponent('scalebutton',{
      this.components.scalebutton.resetCounter();
      //apply the scale delta till we hit the min or max
      if(target != null){
-       let nextScale = currentScale + entity.components.scalebutton.data.scaleDelta;
-       console.log("lets see: " + nextScale + " " + this.components.scalebutton.currentScale + " " + entity.components.scalebutton.data.scaleDelta);
+       let nextScale = entity.components.scalebutton.data.currentScale + entity.components.scalebutton.data.scaleDelta;
+       console.log("lets see: " + nextScale + " " + entity.components.scalebutton.data.currentScale + " " + entity.components.scalebutton.data.scaleDelta);
        if(nextScale > entity.components.scalebutton.scaleMax){
          nextScale = entity.components.scalebutton.scaleMax;
        }
@@ -30,6 +32,7 @@ AFRAME.registerComponent('scalebutton',{
          nextScale = entity.components.scalebutton.scaleMin;
        }
        TweenMax.to(target.object3D, 0.3, {three:{scaleX:nextScale, scaleY:nextScale, scaleZ:nextScale}, ease:Sine.easeIn});
+       entity.components.scalebutton.data.currentScale = nextScale;
      }
    })
  },
@@ -44,8 +47,8 @@ AFRAME.registerComponent('scalebutton',{
   whenAssociated: function(event){
     //we're assuming a uniform scale to start but we'll be applying one anyway
     target = event.detail.cloneEntity;
-    currentScale = target.object3D.scale.x;
-    console.log("CScale: " + currentScale);
+    entity.components.scalebutton.data.currentScale = 0.6;
+    console.log("CScale: " + entity.components.scalebutton.data.currentScale);
   },
   whenDisassociated: function(event){
     target = null;
