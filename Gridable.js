e8c6414403a026656348fd2f2efbd1abe3d40876
Glitch (pb250-lily-feature) <none>
2021-06-11 19:21:12 +0000

    👔😋 Checkpoint ./Gridable.js:28363934/556

--- Gridable.js
+++ Gridable.js
@@ -1,12 +1,23 @@
-var lastPlacement;
+var lastPlacement = {x:0, y:0, z:0};
 const gridX = 5;
 const gridY = 5;
+const origin = {x:0,y:0,z:0};
+const endPoint = {x: 10, y:10, z:10};
+var factorX = 0;
+var factorY = 0;
 
 AFRAME.registerComponent('examinable',{
   schema: {
-    firstPosition : {type: 'vec3',default:{x:0, y:0, z:0}}
+    firstPosition : {type: 'vec3',default:{x:0, y:0, z:0}},
+    isGridded: {type: 'bool', default:false}
   },
+  
   init: function(){
+    this.el.data.isGridded = false;
+    this.el.data.firstPosition = this.el.components.position;
+  },
+  toggleGrid: function(){
     
+    this.el.setAttribute('position', {});
   }
 });
