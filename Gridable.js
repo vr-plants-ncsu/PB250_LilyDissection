0109eb6c5be73449b81aa3c724086d7e52cffc19
Glitch (pb250-lily-feature) <none>
2021-06-11 19:45:09 +0000

    ☎️🎎 Checkpoint ./Gridable.js:28363934/809 ./index.html:28363934/98

--- Gridable.js
+++ Gridable.js
@@ -6,7 +6,7 @@ const endPoint = {x: 10, y:10, z:10};
 var factorX = 0;
 var factorY = 0;
 
-AFRAME.registerComponent('examinable',{
+AFRAME.registerComponent('gridable',{
   schema: {
     firstPosition : {type: 'vec3',default:{x:0, y:0, z:0}},
     isGridded: {type: 'bool', default:false}
@@ -16,16 +16,29 @@ AFRAME.registerComponent('examinable',{
     this.el.data.isGridded = false;
     this.el.data.firstPosition = this.el.components.position;
     
+    window.addEventListener('keydown', this.onKeydown);
+    
     factorX = (1/gridX) * (0.5);
     factorY = (1/gridY) * (0.5);
   },
   toggleGrid: function(){
+    if(!this.data.isGridded){
     //go row first, column second
     if(lastPlacement.x < (endPoint.x - factorX)){
       //move down
       lastPlacement.y += (factorY * 2);
     }
     lastPlacement.x += (factorX * 2);
-    TweenMax.to(newEntity.object3D, 0.3, {three:{positionX: lastPlacement.x, positionY: lastPlacement.y,positionZ: lastPlacement.z}, ease:Sine.easeIn});
+    TweenMax.to(this.el.object3D, 0.4, {three:{positionX: lastPlacement.x, positionY: lastPlacement.y,positionZ: lastPlacement.z}, ease:Sine.easeIn});
+    }
+    if(this.data.isGridded){
+      TweenMax.to(this.el.object3D, 0.4, {three:{positionX: this.data.firstPosition.x, positionY: this.data.firstPosition.y,positionZ: this.data.firstPosition.z}, ease:Sine.easeIn});
+      lastPlacement = {x:0, y:0, z:0};
+    }
+  },
+   onKeydown: function (evt) {
+    var shortcutPressed = evt.keyCode === 68;
+    if (!shortcutPressed) { return; }
+    this.toggleGrid();
   }
 });
