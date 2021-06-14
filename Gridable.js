ca4b00bc4e3672003b7e60f332961e6ff1a77d78
Glitch (pb250-lily-feature) <none>
2021-06-14 19:14:35 +0000

    💏🤑 Checkpoint ./Gridable.js:28363934/613

--- Gridable.js
+++ Gridable.js
@@ -15,33 +15,38 @@ AFRAME.registerComponent('gridable',{
   init: function(){
     this.data.isGridded = false;
     this.data.firstPosition = this.el.components.position;
+    var comp = this;
     
-    window.addEventListener('keydown', this.onKeydown);
+    window.addEventListener('keydown', function(evt){
+      //the D key in decimol ascii
+      var shortcutPressed = evt.keyCode === 68;
+      if (!shortcutPressed){
+        return;
+      }
+      comp.toggleGrid();
+    });
     
     factorX = (1/gridX) * (0.5);
     factorY = (1/gridY) * (0.5);
   },
   toggleGrid: function(){
+    var entity = this.el;
     if(!this.data.isGridded){
-      var entity = this.el;
     //go row first, column second
     if(lastPlacement.x < (endPoint.x - factorX)){
       //move down
       lastPlacement.y += (factorY * 2);
     }
     lastPlacement.x += (factorX * 2);
+    console.log("Gridding");
     TweenMax.to(entity.object3D, 0.4, {three:{positionX: lastPlacement.x, positionY: lastPlacement.y,positionZ: lastPlacement.z}, ease:Sine.easeIn});
     this.data.isGridded = true;
     }
     if(this.data.isGridded){
+      console.log("Ungridding");
       TweenMax.to(entity.object3D, 0.4, {three:{positionX: this.data.firstPosition.x, positionY: this.data.firstPosition.y,positionZ: this.data.firstPosition.z}, ease:Sine.easeIn});
       lastPlacement = {x:0, y:0, z:0};
       this.data.isGridded = false;
     }
-  },
-   onKeydown: function (evt) {
-    var shortcutPressed = evt.keyCode === 68;
-    if (!shortcutPressed) { return; }
-    this.toggleGrid;
   }
 });
