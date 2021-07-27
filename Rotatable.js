c35a13b5dc0532244129b37aafea14ff3d819149
Glitch (pb250-lily-feature) <none>
2021-07-27 20:26:25 +0000

    🎃🏏 Checkpoint ./Rotatable.js:28363934/2769

--- Rotatable.js
+++ Rotatable.js
@@ -1,29 +1,15 @@
-var isRotating = false;
-var element;
-
 AFRAME.registerComponent('rotatable',{
   schema: {
+    isRotating:{type:'bool', default: false}
   },
   init: function(){
+    let entity = this.el;
+    
     this.el.addEventListener('mousedown', this.startRotation);
     document.addEventListener('mousedown', this.onMouseDown)
     document.addEventListener('mouseup', this.onMouseUp);
-    document.addEventListener('mousemove', this.onMouseMove);
-    
-    element = this.el;
-  },
-  startRotation: function(){
-    isRotating = true;
-  },
-  onMouseDown: function (evt) {
-    this.oldClientX = evt.clientX;
-    this.oldClientY = evt.clientY;
-  },
-  onMouseUp: function(){
-    isRotating = false;
-  },
-  onMouseMove: function(evt){
-    if(isRotating){
+    document.addEventListener('mousemove', function(evt){
+      if(this.data.isRotating){
       var dX;
     var dY;
     //TODO find our pivot
@@ -33,14 +19,25 @@ AFRAME.registerComponent('rotatable',{
     dX = this.oldClientX - evt.clientX;
     dY = this.oldClientY - evt.clientY;
     //we can tweak these as speeds
-    element.object3D.rotation.y -= dX / 100;
-    element.object3D.rotation.z -= dY / 200;
+    entity.object3D.rotation.y -= dX / 100;
+    entity.object3D.rotation.z -= dY / 200;
 
     // Clamp x rotation to [-90,90]
-    element.object3D.rotation.z = Math.min(Math.max(-Math.PI / 2, element.object3D.rotation.z), Math.PI / 2);
+    entity.object3D.rotation.z = Math.min(Math.max(-Math.PI / 2, entity.object3D.rotation.z), Math.PI / 2);
 
     this.oldClientX = evt.clientX;
     this.oldClientY = evt.clientY;
-    }
+    }});
+    
+  },
+  startRotation: function(){
+    this.data.isRotating = true;
+  },
+  onMouseDown: function (evt) {
+    this.oldClientX = evt.clientX;
+    this.oldClientY = evt.clientY;
+  },
+  onMouseUp: function(){
+    this.data.isRotating = false;
   }
 });
